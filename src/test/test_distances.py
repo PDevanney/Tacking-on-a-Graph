8c7a832879c9de7520ff1a629235ca780e1bbea4
Patrick Devanney <2329979d@student.gla.ac.uk>
2020-10-25 16:34:59 +0000

    Added context.py to allow tests to continue running in current directory. Updated current tests

--- src/test/test_distances.py
+++ src/test/test_distances.py
@@ -2,36 +2,37 @@ import unittest
 import networkx as nx
 
 from unittest import TestCase
-from distances import *
+from context import distances
+
 
 class Test(TestCase):
     def setUp(self):
         # Create a Pappus Graph +  Add a node to Simulate a disconnected Graph
         self.G = nx.pappus_graph()
         self.G.add_node(43)
-
         self.C = nx.complete_graph(3)
 
     def test_current_distance_to_target_connected(self):
-        d = current_distance_to_target(self.G, 1, 3)
+        d = distances.current_distance_to_target(self.G, 1, 3)
         self.assertEqual(2, d)
 
     def test_current_distance_to_target_disconnected(self):
-        d = current_distance_to_target(self.G, 1, 43)
+        d = distances.current_distance_to_target(self.G, 1, 43)
         self.assertEqual(-1, d)
 
     def test_populate_distance_table(self):
-        t = populate_distance_table(self.G, 1)
-        tt = {0: 1, 1: 0, 2: 1, 3: 2, 4: 3, 5: 2, 6: 3, 7: 2, 8: 1, 9: 2, 10: 3, 11: 4, 12: 3, 13: 2, 14: 3, 15: 4, 16: 3, 17: 2, 43: -1}
+        t = distances.populate_distance_table(self.G, 1)
+        tt = {0: 1, 1: 0, 2: 1, 3: 2, 4: 3, 5: 2, 6: 3, 7: 2, 8: 1, 9: 2, 10: 3, 11: 4, 12: 3, 13: 2, 14: 3, 15: 4,
+              16: 3, 17: 2, 43: -1}
         self.assertEqual(t, tt)
 
     def test_populate_distance_table_connected(self):
-        t = populate_distance_table(self.C, 1)
-        tt = {0: 1, 1: 0, 2:1}
+        t = distances.populate_distance_table(self.C, 1)
+        tt = {0: 1, 1: 0, 2: 1}
         self.assertEqual(t, tt)
 
     def test_populate_distance_table_disconnected(self):
         self.C.add_node(3)
-        t = populate_distance_table(self.C, 1)
+        t = distances.populate_distance_table(self.C, 1)
         tt = {0: 1, 1: 0, 2: 1, 3: -1}
         self.assertEqual(t, tt)
