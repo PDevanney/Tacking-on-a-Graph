d41ad62d81216154b18e1b629bd69eaa11c74976
PDevanney <patrickdevanney28@gmail.com>
2021-01-08 00:18:50 +0000

    Add appropriate test files

--- src/tests/test_distances.py
+++ src/tests/test_distances.py
@@ -1,4 +1,5 @@
 import unittest
+
 import networkx as nx
 
 from unittest import TestCase
@@ -37,3 +38,6 @@ class TestDistances(TestCase):
         t = distances.populate_distance_table(self.C, 1)
         tt = {0: 1, 1: 0, 2: 1, 3: -1}
         self.assertEqual(tt, t)
+
+if __name__ == '__main__':
+    unittest.main()
